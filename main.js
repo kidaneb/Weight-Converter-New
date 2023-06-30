72d0076de0277167245496bc7d7cdd7115cf7d44
kidaneb <105313373+kidaneb@users.noreply.github.com>
2023-06-30 22:21:17 +0300

    simple weight converter into general converter

--- main.js
+++ main.js
@@ -1,5 +1,183 @@
 let toGram1;
 let toGram2;
+
+
+// const fromDropdown = document.getElementById('from-dropdown');
+const toDropdown = document.getElementById('to-dropdown');
+const unitContainer = document.getElementById('unit-container');
+const unitDropdown = document.getElementById('units-dropdown');
+
+unitContainer.addEventListener('click', function(){
+    unitDropdown.style.display = 'flex';
+    unitDropdown.style.flexDirection = 'column';
+    unitDropdown.style.alignItems = 'center';
+})
+unitContainer.addEventListener('mouseleave', function(){
+    unitDropdown.style.display = 'none';
+})
+
+document.getElementById('area').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Area';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('pressure').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Pressure';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('mass').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Mass';    
+    event.stopPropagation();
+
+    const fromButton = document.getElementById('from-button');
+    fromButton.lastChild.remove();
+    
+    // FROM DROP DOWNS
+
+    const fromDropdown = document.createElement('div');
+    fromDropdown.className = 'from-dropdown';
+    fromDropdown.id = 'from-dropdown';
+    
+    fromButton.appendChild(fromDropdown);
+
+    const fromGram = document.createElement('button');
+    fromGram.innerHTML = 'Gram';
+    fromGram.className = 'from-gram button';
+
+    const fromPound = document.createElement('button');
+    fromPound.innerHTML = 'Pound';
+    fromPound.className = 'from-pound button';
+
+    const fromOunce = document.createElement('button');
+    fromOunce.innerHTML = 'Ounce';
+    fromOunce.className = 'from-ounce button';
+
+    const fromKg = document.createElement('button');
+    fromKg.innerHTML = 'kiloGram';
+    fromKg.className = 'from-kg button';
+
+    fromDropdown.appendChild(fromGram);
+    fromDropdown.appendChild(fromPound);
+    fromDropdown.appendChild(fromOunce);
+    fromDropdown.appendChild(fromKg);
+
+    // TO DROP DOWNS
+    const toGram = document.createElement('button');
+    toGram.innerHTML = 'Gram';
+    toGram.className = 'to-gram button';
+
+    const toPound = document.createElement('button');
+    toPound.innerHTML = 'Pound';
+    toPound.className = 'to-pound button';
+
+    const toOunce = document.createElement('button');
+    toOunce.innerHTML = 'Ounce';
+    toOunce.className = 'to-ounce button';
+
+    const toKg = document.createElement('button');
+    toKg.innerHTML = 'kiloGram';
+    toKg.className = 'to-kg button';
+
+    toDropdown.appendChild(toGram);
+    toDropdown.appendChild(toPound);
+    toDropdown.appendChild(toOunce);
+    toDropdown.appendChild(toKg);
+    
+})
+
+  
+
+document.getElementById('length').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Length';
+    event.stopPropagation();
+
+    const fromButton = document.getElementById('from-button');
+    fromButton.lastChild.remove();
+
+    // FROM DROP DOWNS
+    
+    const fromDropdown = document.createElement('div');
+    fromDropdown.className = 'from-dropdown';
+    fromDropdown.id = 'from-dropdown';
+    
+    fromButton.appendChild(fromDropdown);
+
+    const fromMeter = document.createElement('button');
+    fromMeter.innerHTML = 'Meter';
+    fromMeter.className = 'from-meter button';
+
+    const fromFeet = document.createElement('button');
+    fromFeet.innerHTML = 'Feet';
+    fromFeet.className = 'from-feet button';
+
+    const fromYard = document.createElement('button');
+    fromYard.innerHTML = 'Yard';
+    fromYard.className = 'from-yard button';
+
+    const fromKm = document.createElement('button');
+    fromKm.innerHTML = 'kiloMeter';
+    fromKm.className = 'from-km button';
+
+    fromDropdown.appendChild(fromMeter);
+    fromDropdown.appendChild(fromFeet);
+    fromDropdown.appendChild(fromYard);
+    fromDropdown.appendChild(fromKm);
+    
+})
+
+document.getElementById('temprature').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Temprature';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('volume').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Volume';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('time').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Time';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('energy').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Energy';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('speed').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Speed';
+    event.stopPropagation();
+    
+})
+
+document.getElementById('digital-storage').addEventListener('click', function(event){
+    unitDropdown.style.display = 'none';
+    document.getElementById('unit').innerHTML = 'Digital Storage';
+    event.stopPropagation();
+    
+})
+
+
+
+
+
 fromInput = document.getElementById('from-input');
 toInput = document.getElementById('to-input');
 fromButton = document.getElementById("from-button");
